37490b9aa8244391c48692659c3ef7ee97fe6361
Lucas Nunes Carvalho <lucasnc10@hotmail.com>
2024-06-21 23:51:55 -0300

    NavLink Componente, Order component and the tableComponent, Pagination Component and all of this it's just the html, with not ts or js logic.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,19 +3,27 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/App'
 import { AuthLayout } from './pages/_layouts/Auth'
 import { DashBoard } from './pages/app/Dashboard'
+import { Orders } from './pages/app/orders/Oders'
 import { SignIn } from './pages/auth/Sign-in'
+import { SignUp } from './pages/auth/Sign-up'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [{ path: '/', element: <DashBoard /> }],
+    children: [
+      { path: '/', element: <DashBoard /> },
+      { path: '/orders', element: <Orders /> },
+    ],
   },
 
   {
     path: '/',
     element: <AuthLayout />,
-    children: [{ path: '/sign-in', element: <SignIn /> }],
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
+    ],
   },
   // { path: '/', element: <DashBoard /> },
   // { path: '/sign-in', element: <SignIn /> },
